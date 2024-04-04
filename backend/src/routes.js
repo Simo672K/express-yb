b8acb0d76fa6e582f1fe735d13b2644f56e001fe
Simo672k <mohammed.hakmiii555@gmail.com>
2024-04-04 13:24:13 +0200

    backend: definnig api routes and mock data

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const content = JSON.parse(fs.readFileSync(`${__dirname}/mock.json`));
+
+router.get("/recipes", (req, res) => {
+  res.json(content);
+});
+
+router.post("/recipes", (req, res) => {
+  console.log(req.body);
+  res.status(201).json(req.body);
+});
+
+router.get("/recipes/:id", (req, res) => {
+  const recipeId = +req.params.id;
+  if (content.length >= recipeId && recipeId > 0) {
+    res.send(content[recipeId - 1]);
+    return;
+  }
+  res.status(404).send("Recipe not found!");
+});
+
+router.delete("/recipes/:id", (req, res) => {});
+
+router.put("/recipes/:id", (req, res) => {});
+
 module.exports = router;
