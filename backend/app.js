518c274cd5b01dc353a6218277b12734d03be63d
Simo672k <mohammed.hakmiii555@gmail.com>
2024-04-04 13:25:12 +0200

    backend: using routes and json middleware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const routes = require("./src/routes");
 app = express();
 
+// middlewares
+app.use(express.json());
+app.use("/api", routes);
+
 // running the app
 const port = 3000;
 app.listen(port, () => {
