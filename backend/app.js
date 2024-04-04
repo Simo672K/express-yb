5fc6e30723de00d80082068e7ed6316889e7f710
Simo672k <mohammed.hakmiii555@gmail.com>
2024-04-04 17:48:02 +0200

    backend: mongodb store initializtion, recipe schema model

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
+// dependencies
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+console.log(process.env.MONGODB_URL);
+
+const store = require("./src/store");
 const routes = require("./src/routes");
+
 app = express();
 
 // middlewares
 app.use(express.json());
 app.use("/api", routes);
+console.log();
 
 // running the app
 const port = 3000;
